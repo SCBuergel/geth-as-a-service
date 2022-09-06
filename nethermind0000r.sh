78a45648ac38fd991f48d22af1323fbe96d0abe0
Sebastian Bürgel <s.buergel@gmail.com>
2022-09-06 20:14:12 +0200

    tee

--- nethermind0000r.sh
+++ nethermind0000r.sh
@@ -6,7 +6,7 @@ NETHERMIND_CONFIG="mainnet_pruned"
 NETHERMIND_JSONRPCCONFIG_ENABLED=true
 NETHERMIND_JSONRPCCONFIG_HOST="0.0.0.0"
 NETHERMIND_HEALTHCHECKSCONFIG_ENABLED="true"
-' >> /home/nethermind/.env
+' | sudo tee /home/nethermind/.env
 sudo bash -c 'echo "nethermind soft nofile 1000000" > /etc/security/limits.d/nethermind.conf'
 sudo bash -c 'echo "nethermind hard nofile 1000000" >> /etc/security/limits.d/nethermind.conf'
 wget https://raw.githubusercontent.com/SCBuergel/geth-as-a-service/main/nethermind.service
